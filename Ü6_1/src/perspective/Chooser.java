083905696818e2579d3faa8381809ecd4905ef3a
flying sheep <flying-sheep@web.de>
2012-06-11 13:15:30 +0200

    neues projekt für Ü6.1 hinzugefügt

---
+++ Ü6_1/src/perspective/Chooser.java
@@ -0,0 +1,36 @@
+package perspective;
+
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+public class Chooser extends Frame {
+	
+	public Chooser() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Chooser(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Chooser(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Chooser(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
